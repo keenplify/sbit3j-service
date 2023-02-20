25da9397c4437df24777f42e6e43f414c0b09d1c
keenplify <keenplify>
2023-02-21 02:00:06 +0800

    FEAT - Polyfill

--- server.ts
+++ server.ts
@@ -13,6 +13,14 @@
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
+const fetch = require('node-fetch')
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch
+  globalThis.Headers = fetch.Headers
+  globalThis.Request = fetch.Request
+  globalThis.Response = fetch.Response
+}
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
