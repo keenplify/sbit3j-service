b0263a6200624c186dc26677322456cd8611074e
keenplify <keenplify>
2023-04-25 23:16:47 +0800

    FIX - removed auth guard in global

--- start/v1/global.ts
+++ start/v1/global.ts
@@ -5,12 +5,8 @@ Route.group(() => {
     .apiOnly()
     .only(['index', 'show'])
 
-  Route.group(() => {
-    Route.resource('workout-library', 'WorkoutLibrariesController')
-      .apiOnly()
-      .only(['index', 'show'])
-    Route.resource('file-upload', 'FileUploadsController').apiOnly().only(['store'])
-  }).middleware('auth')
+  Route.resource('workout-library', 'WorkoutLibrariesController').apiOnly().only(['index', 'show'])
+  Route.resource('file-upload', 'FileUploadsController').apiOnly().only(['store'])
 })
   .prefix('v1/global')
   .as('v1.global')
